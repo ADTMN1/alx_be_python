839effc8b2d3b5c2032203c5eedcd21eb68c4f5a
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-05-24 22:21:28 -0700

    simple_interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# simple_interest.py
+
+principal = 1000      # initial investment amount
+rate = 0.05           # annual interest rate (5%)
+time = 3              # time in years
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
