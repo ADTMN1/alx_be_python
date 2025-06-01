3e60d8446e3461c182d8713e8dfa2125515e663d
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-06-01 12:08:40 -0700

    integer array

--- Practices/test.py
+++ Practices/test.py
@@ -14,37 +14,81 @@
 #             pair = tuple(sorted((num, complement)))
 #             pairs.add(pair)
 #         seen.add(num)
+# #
+# #     return list(pairs)
+# #
+# # # Test
+# # input_list = [1, 2, 3, 2, 4, 5, 0, -1, 4]
+# # target_sum = 4
+# #
+# # result = find_pairs(input_list, target_sum)
+# # print(result)
 #
-#     return list(pairs)
+#
+# # Problem:
+#
+# # Write a function that takes a sentence as input and
+# # returns the longest word in the sentence.
+# # If there are multiple words of the same maximum length, return the first one.
+#
+# def longest_word(sentence):
+#     words = sentence.split()
+#     max_word = ""
+#
+#     for word in words:
+#         # Remove punctuation (optional, depending on the requirement)
+#         clean_word = ''.join(char for char in word if char.isalnum())
+#         if len(clean_word) > len(max_word):
+#             max_word = clean_word
+#
+#     return max_word
+#
 #
 # # Test
-# input_list = [1, 2, 3, 2, 4, 5, 0, -1, 4]
-# target_sum = 4
-#
-# result = find_pairs(input_list, target_sum)
+# sentence = "Artificial intelligence is revolutionizing the world"
+# result = longest_word(sentence)
 # print(result)
 
 
-# Problem:
+#  Problem Statement:
+# Given an integer array nums,
+# return all the triplets [nums[i], nums[j], nums[k]] such that:
+#
+# i != j, i != k, and j != k
+#
+# nums[i] + nums[j] + nums[k] == 0
+#
+# Note: The solution set must not contain duplicate triplets.
 
-# Write a function that takes a sentence as input and
-# returns the longest word in the sentence.
-# If there are multiple words of the same maximum length, return the first one.
+def three_sum(nums):
+    nums.sort()
+    result = []
 
-def longest_word(sentence):
-    words = sentence.split()
-    max_word = ""
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
 
-    for word in words:
-        # Remove punctuation (optional, depending on the requirement)
-        clean_word = ''.join(char for char in word if char.isalnum())
-        if len(clean_word) > len(max_word):
-            max_word = clean_word
+        left, right = i + 1, len(nums) - 1
 
-    return max_word
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
 
+            if total < 0:
+                left += 1
+            elif total > 0:
+                right -= 1
+            else:
+                result.append([nums[i], nums[left], nums[right]])
 
-# Test
-sentence = "Artificial intelligence is revolutionizing the world"
-result = longest_word(sentence)
-print(result)
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+
+    return result
+
+# Function call with output
+print(three_sum([-1, 0, 1, 2, -1, -4]))
