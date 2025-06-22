fca23358aeab139de63c1e9bff21965624b950f1
ADTMN1 <163424703+ADTMN1@users.noreply.github.com>
2025-06-22 05:21:33 -0700

    polmorphism demo

--- oop/main.py
+++ oop/main.py
@@ -1,5 +1,18 @@
 from book_class import Book as BookWithYear
 from library_system import Book, EBook, PrintBook, Library
+from polymorphism_demo import Shape, Rectangle, Circle
+
+def main():
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
+
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+
+if __name__ == "__main__":
+    main()
 
 def test_library():
     my_library = Library()
